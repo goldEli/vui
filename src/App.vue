095cbb6e304011038560b282620f5522aa7f0053
miaoyu <miaoyu@sefonsoft.com>
2020-11-16 17:44:00 +0800

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-	<h1>hello</h1>
-	<router-view />
+  <h1>导航栏</h1>
+  <router-link to="/">test</router-link> | 
+  <router-link to="/xxx">test2</router-link>
+  <hr />
+  <router-view />
 </template>
 
 <script>
-
 export default {
-	naem: "App"
+  naem: "App",
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import Test from "./Test.vue";
+import Test2 from "./Test2.vue";
 import { createWebHashHistory, createRouter } from "vue-router";
 
 const history = createWebHashHistory();
 const router = createRouter({
   history,
-  routes: [{ path: "/", component: Test }]
+  routes: [
+    { path: "/", component: Test },
+    { path: "/xxx", component: Test2 },
+  ]
 });
 
 const app = createApp(App);
