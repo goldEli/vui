d008c98caa01c346bf3b5fd834de8eb25b0a36cc
miaoyu <miaoyu@sefonsoft.com>
2020-11-25 13:48:03 +0800

    optimize routing switching

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,20 @@
 </template>
 
 <script>
-import {provide, reactive, ref} from "vue"
+import { provide, reactive, ref } from "vue";
+import router from "./router";
 export default {
   name: "App",
   setup() {
-    const windowWidth = window.document.documentElement.clientWidth
-    const menuVisible = ref(windowWidth > 500 ? true : false)
-    provide("menuVisible", menuVisible)
-  }
+    const windowWidth = window.document.documentElement.clientWidth;
+    const menuVisible = ref(windowWidth > 500 ? true : false);
+    provide("menuVisible", menuVisible);
+    router.afterEach(() => {
+      if (windowWidth <= 500) {
+        menuVisible.value = false;
+      }
+    });
+  },
 };
 </script>
 
