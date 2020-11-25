1504504e7a788d5fe2329db763968321babe0b0c
miaoyu <miaoyu@sefonsoft.com>
2020-11-25 13:57:55 +0800

    document structure done

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,10 @@
 import Home from "./views/Home.vue"
 import Doc from "./views/Doc.vue"
 import SwitchDemo from "./components/SwitchDemo.vue"
+import ButtonDemo from "./components/ButtonDemo.vue";
+import DialogDemo from "./components/DialogDemo.vue";
+import TabsDemo from "./components/TabsDemo.vue";
+import DocDemo from "./components/DocDemo.vue";
 
 import { createWebHashHistory, createRouter } from "vue-router";
 const history = createWebHashHistory();
@@ -10,7 +14,11 @@ const router = createRouter({
     { path: "/", component: Home },
     {
       path: "/doc", component: Doc, children: [
-        { path: "switch", component: SwitchDemo }
+        { path: "", component: DocDemo },
+        { path: "switch", component: SwitchDemo },
+        { path: "tabs", component: TabsDemo },
+        { path: "dialog", component: DialogDemo },
+        { path: "button", component: ButtonDemo },
       ]
     },
   ]
