92d57e5bd12fd8f0697702ecda7ff39419d55d28
miaoyu <miaoyu@sefonsoft.com>
2020-11-26 15:15:30 +0800

    modify Button

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,11 +1,17 @@
 <template>
-  <Button>hello</Button>
+  <Button @click="onClick" @focus="onClick" @mouseover="onClick">hello</Button>
 </template> 
 <script lang="ts">
 import Button from '../lib/Button.vue'
 export default {
   components: {
     Button
+  },
+  setup() {
+    const onClick = () => {
+      console.log("h1")
+    }
+    return {onClick}
   }
 }
 </script>
