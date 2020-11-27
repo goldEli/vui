72c7f735b41245c8e75f0c6102998d79bc1d2461
miaoyu <miaoyu@sefonsoft.com>
2020-11-27 11:22:16 +0800

    add Tabs

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,3 +1,18 @@
 <template>
-  <div>Tabs 示例</div>
+  <h1>Tabs 示例</h1>
+  <Tabs>
+    <Tab title="tab1"> Tab1 content </Tab>
+    <Tab title="tab2"> Tab2 content </Tab>
+    <Tab title="tab3"> Tab3 content </Tab>
+  </Tabs>
 </template> 
+<script lang="ts">
+import Tabs from "../lib/Tabs.vue";
+import Tab from "../lib/Tab.vue";
+export default {
+  components: {
+    Tabs,
+    Tab,
+  },
+};
+</script>
---
+++ src/lib/Tab.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <span>{{title}}</span>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ""
+    }
+  }
+}
+</script>
