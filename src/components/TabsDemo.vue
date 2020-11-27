0b5c63dcc26c6c4d6e1c6cb6fcf27bf310acf63a
miaoyu <miaoyu@sefonsoft.com>
2020-11-27 13:51:00 +0800

    make tab switchable

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,9 +1,9 @@
 <template>
   <h1>Tabs 示例</h1>
-  <Tabs>
-    <Tab title="tab1"> Tab1 content </Tab>
-    <Tab title="tab2"> Tab2 content </Tab>
-    <Tab title="tab3"> Tab3 content </Tab>
+  <Tabs defaultActiveKey="3">
+    <Tab title="tab1" key="1"> Tab1 content </Tab>
+    <Tab title="tab2" key="2"> Tab2 content </Tab>
+    <Tab title="tab3" key="3"> Tab3 content </Tab>
   </Tabs>
 </template> 
 <script lang="ts">
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,6 +1,53 @@
 
 <template>
   <div>
-    <slot />
+    <component
+      @click="currentTab.data = c.children?.default?.()"
+      v-for="c in defaults"
+      v-bind:key="c.key"
+      v-bind:is="c"
+    ></component>
+    <div>
+      <component
+        v-for="(c, idx) in currentTab.data"
+        v-bind:key="idx"
+        v-bind:is="c"
+      ></component>
+    </div>
   </div>
 </template>
+<script lang="ts">
+import { reactive, Ref, ref } from "vue";
+import Tab from "./Tab.vue";
+export default {
+  props: {
+    defaultActiveKey: {
+      type: String,
+      default: "",
+    },
+  },
+  setup(props, context) {
+    // console.log(context.slots.default());
+    const defaults = context.slots.default();
+    const currentTab = reactive({ data: "" });
+
+    const changeActiveKey = (event) => {
+      console.log(event.target);
+    };
+
+    for (let d of defaults) {
+      if (d.type !== Tab) {
+        throw new Error("Tabs's children has type which is not Tab");
+      }
+      if (d.key === props.defaultActiveKey) {
+        currentTab.data = d.children?.default?.();
+      }
+    }
+    return {
+      defaults,
+      currentTab,
+      changeActiveKey,
+    };
+  },
+};
+</script>
