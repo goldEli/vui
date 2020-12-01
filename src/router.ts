0ab9d154c6a53bc9d10dbf65cfe3957471bc8e1c
miaoyu <miaoyu@sefonsoft.com>
2020-12-01 20:37:04 +0800

    add new routers

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,8 @@
 import Home from "./views/Home.vue"
 import Doc from "./views/Doc.vue"
+import Intro from "./views/Intro.vue";
+import Install from "./views/Install.vue"
+import GetStarted from "./views/GetStarted.vue";
 import SwitchDemo from "./components/SwitchDemo.vue"
 import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
@@ -14,7 +17,10 @@ const router = createRouter({
     { path: "/", component: Home },
     {
       path: "/doc", component: Doc, children: [
-        { path: "", component: DocDemo },
+        { path: "", component: Intro },
+        { path: "intro", component: Intro },
+        { path: "install", component: Install },
+        { path: "getStarted", component: GetStarted },
         { path: "switch", component: SwitchDemo },
         { path: "tabs", component: TabsDemo },
         { path: "dialog", component: DialogDemo },
