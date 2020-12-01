5ffdc2d3cc3b4cfda58a1a007a98dc58d00de1e9
miaoyu <miaoyu@sefonsoft.com>
2020-12-01 20:41:46 +0800

    modify content

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,5 +1,8 @@
 <template>
-<div>
-  Intro
-</div>
+  <article>
+    <h1>介绍</h1>
+    <p>VUI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
+    <p>开发此UI库，主要是为了学习 vue 3 新特性，所以请不要在生产环境中使用</p>
+    下一节：<a href="#/doc/install">安装</a>
+  </article>
 </template>
