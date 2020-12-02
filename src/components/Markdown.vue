3b6796de4ae4e61eb89cd7b9725f8eea718160ab
miaoyu <miaoyu@sefonsoft.com>
2020-12-02 15:31:52 +0800

    add markdown compnent

---
+++ src/components/Markdown.vue
@@ -0,0 +1,13 @@
+
+<template>
+  <div class="markdown-body" v-html="md"></div>
+</template>
+<script lang="ts">
+export default {
+  props: {
+    md: {
+      type: String,
+    }
+  }
+};
+</script>
--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,21 @@
 import Home from "./views/Home.vue"
 import Doc from "./views/Doc.vue"
-import Intro from "./views/Intro.vue";
-import Install from "./views/Install.vue"
-import GetStarted from "./views/GetStarted.vue";
 import SwitchDemo from "./components/SwitchDemo.vue"
 import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
-import DocDemo from "./components/DocDemo.vue";
+import Markdown from "./components/Markdown.vue";
+
+import getStartedMD from "./markdown/getStarted.md";
+import installMD from "./markdown/install.md";
+import introMD from "./markdown/intro.md";
 
 import { createWebHashHistory, createRouter } from "vue-router";
+import { h } from "vue";
+
+const create = (md:string) => {
+  return h(Markdown, {md})
+}
 const history = createWebHashHistory();
 const router = createRouter({
   history,
@@ -17,10 +23,10 @@ const router = createRouter({
     { path: "/", component: Home },
     {
       path: "/doc", component: Doc, children: [
-        { path: "", component: Intro },
-        { path: "intro", component: Intro },
-        { path: "install", component: Install },
-        { path: "getStarted", component: GetStarted },
+        { path: "", component: create(introMD) },
+        { path: "intro", component: create(introMD) },
+        { path: "install", component: create(installMD) },
+        { path: "getStarted", component: create(getStartedMD) },
         { path: "switch", component: SwitchDemo },
         { path: "tabs", component: TabsDemo },
         { path: "dialog", component: DialogDemo },
